182f32fea31c384a3808c61ff54ca2c91f9e179b
cobbzac <cobbzac@urbandaleschools.com>
2019-12-17 14:22:54 -0600

    uhh

--- 8.0_Jedi_Training.py
+++ 8.0_Jedi_Training.py
@@ -8,9 +8,23 @@ Don't use that. Sum the numbers individually as shown in the chapter.
 Also, a common mistake is to calculate the average each time through the loop 
 to add the numbers. Finish adding the numbers before you divide.
 '''
+
 a_list = [3,12,3,5,3,4,6,8,5,3,5,6,3,2,4]
 b_list = [4,15,2,7,8,3,1,10,9]
 c_list = [5,10,13,12,5,9,2,6,1,8,8,9,11,13,14,8,2,2,6,3,9,8,10]
+list_total=0
+
+for item in a_list:
+    list_total += item
+print(list_total)
+
+for item in b_list:
+    list_total += item
+print(list_total)
+
+for item in c_list:
+    list_total += item
+print(list_total)
 
 
 
