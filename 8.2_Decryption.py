c4a9e6c133774b30688c11bb4cd722505879adf6
cobbzac <cobbzac@urbandaleschools.com>
2020-01-09 14:12:51 -0600

    camel game

--- 8.2_Decryption.py
+++ 8.2_Decryption.py
@@ -9,5 +9,9 @@ your program 40 times. Use a FOR loop from -20 to +20 to generate all the possib
 Secret Message: Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*
 '''
 
-
+x = 1
+secret_message = "Lxwp{j}~uj}rxw|*)bx~)l{jltnm)}qn)lxmn7)]qn)ox{ln)r|)\][XWP)r}q)x~*"
+for item in secret_message:
+    print(ord(secret_message[:x]))
+    x+=1
 
