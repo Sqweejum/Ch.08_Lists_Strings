42c10b17d91ae099323f754a8dc1387149afbf8d
cobbzac <cobbzac@urbandaleschools.com>
2020-01-13 14:00:36 -0600

    please grade

--- 8.1_Months.py
+++ 8.1_Months.py
@@ -4,7 +4,7 @@ MONTHS PROGRAM
 Write a user-input statement where a user enters a month number 1-12.
 Using the starting string below in your program, print the three month abbreviation 
 for the month number that the user enters. Keep repeating this until the user enters a non 1-12 number to quit.
-Once the user quits, print "Goodbye!"
+Once the user quits, print "Goodbye! "
 
 months = "JanFebMarAprMayJunJulAugSepOctNovDec"
 
